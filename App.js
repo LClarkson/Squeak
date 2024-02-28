47a057a76d7aba3b29b0144c580f9ec0a50b766c
LClarkson <l.j.clarkson@gmail.com>
2024-02-27 17:22:07 -0800

    add squeak button and flip camera button, run first dev build on Pixel3a device

--- App.js
+++ App.js
@@ -1,41 +1,66 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
+import { Audio } from 'expo-av';
 import * as MediaLibrary from 'expo-media-library';
 import Button from './src/components/Button.js';
 
 
 export default function App() {
 
+  // camera state
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
   const [image, setImage] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
   const cameraRef = useRef(null);
 
-  useEffect (() => {
+  // audio state
+  const [sound, setSound] = useState();
+
+  useEffect(() => {
     (async () => {
       MediaLibrary.requestPermissionsAsync();
-      const cameraStatus = await Camera.requestMicrophonePermissionsAsync();
-      setHasCameraPermission(cameraStatus.status === 'granted');
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === "granted");
     })();
   }, []);
 
   const takePicture = async () => {
     if (cameraRef) {
       try {
-        const data = await cameraRef.current.takePictureAsync()
+        const data = await cameraRef.current.takePictureAsync();
         console.log(data);
         setImage(data.uri);
       } catch (err) {
         console.log(err);
       }
     }
+  };
+
+  const playSound = async () => {
+    console.log('Loading sound...');
+    const { sound } = await Audio.Sound.createAsync( require('./assets/squeak.mp3')
+    );
+    setSound(sound);
+    console.log('Playing sound...');
+    await sound.playAsync();
   }
 
   if (hasCameraPermission === false) {
-    return <Text>No Access to Camera</Text>
+    return <Text>No Access to Camera</Text>;
   }
+
+  
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
 
   return (
     <View style={styles.container}>
@@ -44,11 +69,11 @@ export default function App() {
         type={type}
         flashMode={flash}
         ref={cameraRef}
-       >
-        <Text>Take Pics of Your Doggens</Text>
-      </Camera>
-      <View>
-        <Button title={'Take a pic of your doggens'} icon="camera" onPress={takePicture}></Button>
+      ></Camera>
+      <View style={styles.controls}>
+        <Button style={styles.flip} icon="cycle"></Button>
+        <Button style={styles.shutter} icon="controller-record"  onPress={takePicture}></Button>
+        <Button style={styles.noiseMaker} icon="controller-play" onPress={playSound}></Button>
       </View>
     </View>
   );
@@ -57,12 +82,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
   camera: {
-    flex: 1,
+    marginTop: "20%",
+    width: "100%",
+    height: "65%",
     borderRadius: 20,
+  },
+  controls: {
+    flex: 1,
+    flexDirection: "row",
+    width: "85%",
+    justifyContent: "space-around",
   }
+
 });
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,27 +2,27 @@ import * as React from 'react';
 import { Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 
-export default function Button ({title, onPress, icon, color}) {
-    return (
-        <TouchableOpacity onPress={onPress} style={styles.button}>
-            <Entypo name={icon} size={28} color={color ? color : '#f1f1f1'} />
-            <Text style={styles.text}>{title}</Text>
-        </TouchableOpacity>
-    )
+export default function Button({ onPress, icon, color }) {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.button}>
+      <Entypo name={icon} size={58} color={color ? color : "#f1f1f1"} />
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: {
-        height: 80,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginBottom: '15%',
-    },
-    text: {
-        fontWeight: 'bold',
-        fontSize: 16,
-        color: '#f1f1f1',
-        marginLeft: 10
-    }
-})
+  button: {
+    height: 80,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "4%",
+    marginBottom: "5%",
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#f1f1f1",
+    marginLeft: 10,
+  },
+});
